7fa750beb266d0ac7fcf0a920cddc2a1e140f29f
Matte <mszklarz@gmail.com>
2017-07-19 10:29:09 -0400

    unignore dist/ folder

---
+++ dist/scripts/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){function n(){$(this).scrollTop()>500?d.removeClass("hidden"):d.addClass("hidden")}var d=$(".main-nav");n(),$("#hero").length?$(window).scroll(n):d.removeClass("hidden")});
