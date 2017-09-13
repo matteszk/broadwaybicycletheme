3ec9bd426f72d2545d5d069fa8dc302f01fa3910
Matte <mszklarz@gmail.com>
2017-09-12 20:13:32 -0400

    add mobile menu

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+// var $nav = $('.main-nav');
+//
+// function toggleTopNav() {
+//   if ($(this).scrollTop() > 500) {
+//     $nav.removeClass('hidden');
+//   }
+//   else {
+//     $nav.addClass('hidden');
+//   }
+// }
+//
+// toggleTopNav();
+// if (!!$('#hero').length) {
+//   $(window).scroll(toggleTopNav);
+//
+// }
+// else {
+//   $nav.removeClass('hidden');
+// }
+// });
+
+$(document).ready(function() {
+  $('.menu-toggle').click(function(){
+    $('.main-nav').toggleClass('visible');
+  });
+});
