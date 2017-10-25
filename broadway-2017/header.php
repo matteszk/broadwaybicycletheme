3faaaa5e66dc0eb4f569d93e309c9c11c55c31dc
Matte <mszklarz@gmail.com>
2017-10-25 15:20:56 -0400

    exclude new pages from old site nav

--- broadway-2017/header.php
+++ broadway-2017/header.php
@@ -24,8 +24,8 @@
 
 <script src="/js/cufon-yui.js" type="text/javascript"></script>
  <script src="/js/behaviors.js" type="text/javascript"></script>
- 
- 
+
+
  <script type="text/javascript">
 
   var _gaq = _gaq || [];
@@ -39,17 +39,16 @@
   })();
 
 </script>
-  
+
 </head>
 <body <?php body_class(); ?>>
 <div id="content">
 <header>
-			
-		
+
+
 <nav>
 <ul id="topNav">
 <li class="home"><a href="<?php bloginfo('url'); ?>/"><?php bloginfo('name'); ?></a></li>
-
-<?php wp_list_pages('title_li=&exclude=33,34,35,502,572,574,573,575'); ?>
+  <?php wp_list_pages('title_li=&exclude=33,34,35,502,572,574,573,575,4288,4295,4298,4301,4293'); ?>
 </ul>
 </nav></header>
