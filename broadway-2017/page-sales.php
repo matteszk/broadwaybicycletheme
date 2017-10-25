6e14ad136cff6c22d606f2d689636425c72b415a
Matte <mszklarz@gmail.com>
2017-10-25 16:41:07 -0400

    incorporate ACF repeater for brands

--- broadway-2017/page-sales.php
+++ broadway-2017/page-sales.php
@@ -6,59 +6,44 @@ Template Name: Sales
 <?php get_header('2017'); ?>
 
 <div class="relative vh-header">
-  <div class="absolute bg--fixed cover h100 bg-bottom" style="background-image: url('images/rack.jpg');"></div>
+  <div class="absolute bg--fixed cover h100 bg-bottom" style="background-image: url('<?php bloginfo('template_url'); ?>/images/rack.jpg');"></div>
 </div>
 
 <div class="row relative mt3">
-  <div class="col-xs-3 col-xs-offset-1 bg-red">
-    <h1 class="tr"><span class="tilt-l">Sales</span> &amp; <span class="tilt-r">Rentals</span></h1>
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-3 col-md-offset-1 gutter">
+    <h1 class="md-tr"><span class="tilt-l">Sales</span> &amp; <span class="tilt-r">Rentals</span></h1>
     <p>For decades, we’ve been helping people find the right bike for them.  Whether you’re looking for something simple to get to around town, a world-touring adventure machine, or something in the middle, we’re passionate about getting you on a bike that you’re excited to ride, mile after mile.</p>
     <p>We keep a wide variety of new and used parts and accessories on hand to get you properly outfitted for whatever two wheeled trip you’ve got planned.</p>
   </div>
-  <div class="row col-xs-7 col-lg-5 col-xs-offset-1 middle-xs">
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www.surly.com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-surly.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www.marin.com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-marin.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://sim-works.com/">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-simworks.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www..com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-compass.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="https://crustbikes.com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-crust.svg"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="https://paulcomp.com/">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-paul.svg"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www..com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-all-city.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www..com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-brooklyn-bicycle-co.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www..com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-brompton.png"/>
-    </a>
-    <a class="pa-grid col-xs-6 col-sm-4" href="http://www..com">
-      <img alt="" src="<?php bloginfo('template_url'); ?>/images/logo-velo-orange.png"/>
-    </a>
-  </div>
-</div><div class="bg--fixed" style="background-image: url('images/temp-brompton.jpg')"></div>
+  <ul class="mt2 md-mt0 row col-xs-12 col-sm-10 col-sm-offset-1 col-md-7 col-lg-5 col-md-offset-1 middle-xs">
+    <?php
+    if( have_rows('brands') ):
+      while ( have_rows('brands') ) : the_row();
+        $image = get_sub_field('logo');
+        ?>
+        <li class="pa-grid col-xs-6 col-sm-4">
+          <a href="<?php echo get_sub_field('url'); ?>">
+            <img alt="<?php echo $image['alt']; ?>" src="<?php echo $image['url']; ?>"/>
+          </a>
+        </li>
+        <?php
+      endwhile;
+    else :
+      // no rows found
+    endif; ?>
+  </ul>
+</div><div class="bg--fixed" style="background-image: url('<?php bloginfo('template_url'); ?>/images/temp-brompton.jpg')"></div>
 <section class="row section-margin">
-  <div class="col-xs-3 col-xs-offset-1">
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-3 col-md-offset-1">
     <div class="cover image-tall bg-bottom" style="background-image: url(<?php bloginfo('template_url'); ?>/images/brompton-two.jpg);"></div>
   </div>
-  <div class="col-xs-4 pl2">
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-4 gutter mt2 md-mt0">
     <h2>Boston’s Brompton Specialists</h2>
     <p>For many urban dwellers, a folding bike just makes sense.  Brompton Bicycles are the best city folding bikes that we’ve ever seen or serviced.  They fit nicely into cozy apartments, travel with ease on public transportation, and still ride with enough zip and agility to get you around in style.  Since 2009, we’ve been helping people build and customize their ideal Bromptons, and providing expert repairs and service to keep them on the road for years and years.  Come in and let us show you how easily they fit into your life.</p><a class="btn mt1" href="stand-rental.html">Order Yours</a>
   </div>
 </section>
 <div class="section-margin row">
-  <div class="col-xs-4 col-xs-offset-2">
+  <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-4 col-md-offset-2 gutter">
     <h2>Bike Rentals</h2>
     <p>Interested in a Brompton but not ready to take the plunge?  Take one on an extended test ride!  We offer rentals by the day or week, so you can really experience how they ride, fold, and make getting around the city so much simpler.  A helmet, lock, and lights are included with each rental bike.  If you decide to purchase after renting, up to a week of your rental fee will be applied to the cost of your new bike!</p>
     <table>
@@ -72,7 +57,7 @@ Template Name: Sales
       </tr>
     </table>
   </div>
-  <div class="col-xs-4 col-xs-offset-1">
+  <div class="md-show col-md-4 col-md-offset-1">
     <div class="cover image1 bg-top" style="background-image: url(<?php bloginfo('template_url'); ?>/images/brompton-duo.jpg);"></div>
   </div>
 </div>
