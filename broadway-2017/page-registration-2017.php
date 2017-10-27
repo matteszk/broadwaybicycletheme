167a89e060b9390bfd8c55e20d150500707411ce
Matte <mszklarz@gmail.com>
2017-10-27 16:19:08 -0400

    add registration page template

---
+++ broadway-2017/page-registration-2017.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Registration 2017
+*/
+?>
+<?php get_header('2017'); ?>
+
+<div class="main-nav-spacer">
+</div>
+<h1 class="h2 visually-hidden">Classes</h1>
+<section class="relative">
+  <div class="row center-xs">
+    <div class="col-xs-12 col-sm-10 col-md-8 gutter">
+      <h2 class="h1 tilt-l">Event Registration</h2>
+    </div>
+  </div>
+  <div class="row center-xs">
+    <div class="col-xs-12 col-sm-10 col-md-8 gutter">
+      <?php echo do_shortcode('[ESPRESSO_EVENTS]'); ?>
+    </div>
+  </div>
+</section>
+<section class="row section-margin">
+<?php get_footer('2017'); ?>
