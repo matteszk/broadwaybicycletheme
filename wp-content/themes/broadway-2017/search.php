ad4ccfa82df5e33f26f234d70d017f6f9bd6bd6b
Matte <mszklarz@gmail.com>
2017-12-10 14:02:08 -0500

    format search page

--- wp-content/themes/broadway-2017/search.php
+++ wp-content/themes/broadway-2017/search.php
@@ -1,22 +1,21 @@
 <?php get_header(); ?>
 
+<div class="main-nav-spacer"></div>
+
+<div class="row center-xs">
+	<div class="col-xs-12 col-sm-8 col-md-8 col-lg-6 gutter pt1">
+
 		<section>
-
 			<?php if (have_posts()) : ?>
-
 			<article id="post-<?php the_ID(); ?>">
-				<h1>Search Results for &ldquo;<?php the_search_query(); ?>&rdquo;</h1>
+				<h1 class="h2">Search Results for &ldquo;<?php the_search_query(); ?>&rdquo;</h1>
 				<ol>
-
 					<?php while (have_posts()) : the_post(); ?>
-
-					<li>
+					<li class="mt1 mb2">
 						<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 						<?php the_excerpt(); ?>
 					</li>
-
 					<?php endwhile; ?>
-
 				</ol>
 			</article>
 			<nav>
@@ -34,7 +33,10 @@
 			<?php endif; ?>
 
 		</section>
-
-<?php // get_sidebar(); ?>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 gutter pt1">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 
 <?php get_footer(); ?>
